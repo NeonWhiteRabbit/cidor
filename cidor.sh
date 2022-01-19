49557b9556d50f8c424ef202fc3804091cc85f6a
n3onhacks <97998625+n3onhacks@users.noreply.github.com>
2022-01-19 00:38:44 -0600

    Update cidor.sh

--- cidor.sh
+++ cidor.sh
@@ -2,7 +2,7 @@
 #A quick little bashscript by n3on @n3onhacks
 
 #Menu
-echo "CIDOR - Canvas IDOR checker v.1"
+echo "CIDOR - Canvas IDOR checker v.2.0.1"
 echo "Usage: checks domain/[num]/files to check for accessible files via Canvas IDOR (CIDOR) issue"
 echo "...cleaning previous results..."
 
@@ -38,24 +38,24 @@ do
 
 #Pre-results
 echo "Completed. Querying through logs....."
-cat /opt/cidor2/temp/cidor_results.txt | grep -B 22 "50K .........." > /opt/cidor2/temp/cleaner1.txt 
-cat /opt/cidor2/temp/cleaner1.txt | grep "Saving to" | cut -d " " -f 3 | tr -d ":" > /opt/cidor2/temp/cleaner2.txt
-cat /opt/cidor2/temp/cleaner2.txt | awk '{print substr($0,2,length($0)-2)}' > /opt/cidor2/temp/clean.txt
+cat /opt/cidor/temp/cidor_results.txt | grep -B 22 "50K .........." > /opt/cidor/temp/cleaner1.txt 
+cat /opt/cidor/temp/cleaner1.txt | grep "Saving to" | cut -d " " -f 3 | tr -d ":" > /opt/cidor/temp/cleaner2.txt
+cat /opt/cidor/temp/cleaner2.txt | awk '{print substr($0,2,length($0)-2)}' > /opt/cidor/temp/clean.txt
 
 #Final Horrahc
-File="/opt/cidor2/temp/clean.txt"
-echo "#!/bin/bash" > /opt/cidor2/temp/final_results.sh
+File="/opt/cidor/temp/clean.txt"
+echo "#!/bin/bash" > /opt/cidor/temp/final_results.sh
 Lines=$(cat $File)
 for Line in $Lines
 do
-echo "cp /opt/cidor2/temp/'$Line' /opt/cidor2/downloads" > /opt/cidor2/temp/updated_results.txt
-cat /opt/cidor2/temp/updated_results.txt > /opt/cidor2/temp/final_results.sh
-chmod 777 /opt/cidor2/temp/final_results.sh
+echo "cp /opt/cidor/temp/'$Line' /opt/cidor/downloads" > /opt/cidor/temp/updated_results.txt
+cat /opt/cidor/temp/updated_results.txt > /opt/cidor/temp/final_results.sh
+chmod 777 /opt/cidor/temp/final_results.sh
 ./final_results.sh
 
-cat /opt/cidor2/temp/cleaner1.txt
+cat /opt/cidor/temp/cleaner1.txt
 echo "Application/Pdf Files Revieved:"
-ls -l /opt/cidor2/downloads/
+ls -l /opt/cidor/downloads/
 
 
 done
