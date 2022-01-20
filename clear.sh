ffae968e3c8959b572bd009d67bf1882303da455
n3on <97998625+n3onhacks@users.noreply.github.com>
2022-01-20 13:11:55 -0600

    Create clear.sh

---
+++ clear.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm conc_run.sh
+rm conc_done.txt
+rm conc_sort.txt
+rm location1.txt
+rm location2.txt
+rm location.txt
+rm loc_results.txt
+rm microsoft.txt
+rm pdf.txt
+rm testdata.txt
+rm to_conc.txt
+rm -R temp
+mkdir temp
